9c1e6fd4ead0384be30186080f4dab1eedd733eb
snicolaescu <sebastian.nicolaescu@gmail.com>
2018-10-23 17:44:05 +0000

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - now"}
+		res := &response{Message: "Hello World - UONG 2018"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
